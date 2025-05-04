0a5c86135e3fffd14720e28ab7600d5a78a3a21c
JackieYam <ykengzhang@gmail.com>
2025-05-04 15:58:14 +0800

    arrange1 push

--- carbon_wallet_flask/routes/home.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-home = Blueprint('home', __name__)
-
-@home.route('/home', methods=['GET'])
-def home_endpoint():
-   return render_template('home.html')
--- carbon_wallet_flask/routes/records.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-records = Blueprint('records', __name__)
-
-@records.route('/records', methods=['GET'])
-def records_endpoint():
-   return render_template('records.html')
