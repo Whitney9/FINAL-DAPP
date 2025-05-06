07a2ada436daaeb20cb3498f827c90c40e9e66f8
JackieYam <ykengzhang@gmail.com>
2025-05-06 15:56:18 +0800

    adjustment3

--- rent_house_dapp/routes/homepage.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-homepage = Blueprint('homepage', __name__)
-
-@homepage.route('/homepage', methods=['GET'])
-def homepage_endpoint():
-   return render_template('homepage.html')
--- rent_house_dapp/routes/my_rentals.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-my_rentals = Blueprint('my_rentals', __name__)
-
-@my_rentals.route('/my_rentals', methods=['GET'])
-def my_rentals_endpoint():
-   return render_template('my_rentals.html')
